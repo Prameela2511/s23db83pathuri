dac222149225a6c891737028ba6fb679f17095b4
Prameela2511 <143015943+Prameela2511@users.noreply.github.com>
2023-11-27 16:21:07 -0600

    update

--- models/cat.js
+++ models/cat.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose")
 const catSchema = mongoose.Schema({
-    cat_color: String,
-    cat_breed: String,
-    cat_price: Number,
-});
-
-module.exports = mongoose.model('cat', catSchema);
+cat_color: String,
+cat_breed: {type:String,length:15},
+cat_price: {type:Number,min:1000,max:100000}
+})
+module.exports = mongoose.model("cat",catSchema)
--- routes/cat.js
+++ routes/cat.js
@@ -8,16 +8,23 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const cat_controlers= require('../controllers/cat');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET cats */
 router.get('/', cat_controlers.cat_view_all_Page );
 router.get('/detail', cat_controlers.cat_view_one_Page);
 /* GET create cat page */
 router.get('/create', cat_controlers.cat_create_Page);
 /* GET create update page */
-router.get('/update', cat_controlers.cat_update_Page);
+router.get('/update',secured, cat_controlers.cat_update_Page);
 /* GET delete cat page */
 router.get('/delete', cat_controlers.cat_delete_Page);
 
-
 module.exports = router;
-
