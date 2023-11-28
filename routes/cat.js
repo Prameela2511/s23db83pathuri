29f0fff4b397ba47abad84862d56eb50c8cd8205
Prameela2511 <143015943+Prameela2511@users.noreply.github.com>
2023-11-28 16:50:22 -0600

    commit all

--- routes/cat.js
+++ routes/cat.js
@@ -19,12 +19,12 @@ res.redirect("/login");
 }
 /* GET cats */
 router.get('/', cat_controlers.cat_view_all_Page );
-router.get('/detail', cat_controlers.cat_view_one_Page);
+router.get('/detail',secured, cat_controlers.cat_view_one_Page);
 /* GET create cat page */
-router.get('/create', cat_controlers.cat_create_Page);
+router.get('/create', secured, cat_controlers.cat_create_Page);
 /* GET create update page */
 router.get('/update',secured, cat_controlers.cat_update_Page);
 /* GET delete cat page */
-router.get('/delete', cat_controlers.cat_delete_Page);
+router.get('/delete', secured,cat_controlers.cat_delete_Page);
 
 module.exports = router;
